0f43ad60edae1fbe2df30d05f4df6b8531e17f9e
hana825 <hana825@naver.com>
2023-04-12 20:46:07 +0900

    README

---
+++ Hello/Hello/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Hello
+//
+//  Created by mrtMacBook on 2023/04/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        let name = "하나"
+        var nameEng = "hana"
+        print(name)
+        print(nameEng)
+    }
+
+}
+
